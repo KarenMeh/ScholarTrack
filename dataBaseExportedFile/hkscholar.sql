96abae82fc9dbdd1fdfc6a1a3bd69c9b3e4afbe4
itsmetami <prvtapum20@gmail.com>
2024-03-13 19:19:16 +0800

    secound commit of new db

--- dataBaseExportedFile/hkscholar.sql
+++ dataBaseExportedFile/hkscholar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2024 at 04:22 PM
+-- Generation Time: Mar 13, 2024 at 12:17 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
